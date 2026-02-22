11f2ddb60cbe5dfb1eea8da2767a7a02d8c75508
Francisco20-dev <franciscoancamil687@gmail.com>
2026-02-22 20:51:13 -0300

    if y else anidados

---
+++ HolaMundo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+
+public class HolaMundo {
+
+  
+    public static void main(String[] args) {
+        int num1, num2, num3 ;
+        
+     num1 =0;
+     num2=3;
+     
+        if(num2>num1){
+            System.out.println("el numero 2 es mayor que el numero 1");
+           
+        }
+        else{
+            if(num1==num2){
+                System.out.println("los numeros son iguales");
+            }
+            else{
+                if(num1==0){
+                    System.out.println("el numero es igual a cero");
+                }
+                else{
+                    System.out.println("el numero 1 es mayor");
+                }
+            }
+        }
+        
+     
+    }
+    
+}
