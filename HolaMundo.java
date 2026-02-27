44e3e757b0281c351ce4755a62b916b0fbfe8c29
Francisco20-dev <franciscoancamil687@gmail.com>
2026-02-26 21:22:47 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -9,40 +9,48 @@ import java.util.Scanner; // ¡No olvides el import!
 
 public class HolaMundo {
 
-    // ¡Solo dejamos un main!
+     // ¡Solo dejamos un main!
     public static void main(String[] args) {
-        int edad = 0;
-        int membresia = 0;
+         public static void main(String[] args) {
+        int edad=0;
+        int membresia=0;
         
-        Scanner leer = new Scanner(System.in);
-        System.out.println("ingrese su edad:");
-        edad = leer.nextInt();
+        Scanner leer =new Scanner(System.in);
+        System.out.println("ingrese su edad;");
+        edad=leer.nextInt();
         
-        if (edad >= 18) {
+        if (edad>=18){
             System.out.println("eres mayor de edad");
-            System.out.println("tienes membresia?");
+            System.out.println("tines membresia?");
             System.out.println("1) si");
             System.out.println("2) no");
-            membresia = leer.nextInt();
             
-            if (membresia == 1) {
+            membresia=leer.nextInt();
+            
+            if (membresia==1){
                 System.out.println("Eres Adulto VIP: Tienes acceso a la sala 3D y barra libre de snacks.");
-            } else {
-                System.out.println("Eres Adulto Estándar: Tienes acceso a la sala normal.");
+            }else if(membresia==2){
+                System.out.println("Eres Adulto Estándar: Tienes acceso a la sala normal");
+            }else{
+                System.out.println("Error digite un 1 o 2");
             }
-            
-        } else {
+        }else{
             System.out.println("eres menor de edad");
-            System.out.println("tienes membresia?");
+            System.out.println("tines membresia?");
             System.out.println("1) si");
             System.out.println("2) no");
-            membresia = leer.nextInt();
-            
-            if (membresia == 1) {
+            membresia=leer.nextInt();
+            if (membresia==1){
                 System.out.println("Eres Joven VIP: Tienes acceso a la sala de videojuegos antes de la película.");
-            } else {
+            }else if(membresia==2){
                 System.out.println("Eres Joven Estándar: Tienes acceso a la sala normal.");
+                
+            }else{
+                System.out.println("Error digite un 1 o 2");
             }
+    
         }
-    }      
+}      
+    
 }
+
