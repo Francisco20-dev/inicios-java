65f8225fc3682a684a04104ccc4b1d7a562443fd
Francisco20-dev <franciscoancamil687@gmail.com>
2026-02-26 21:03:12 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,35 +5,44 @@
  */
 package holamundo;
 
+import java.util.Scanner; // ¡No olvides el import!
 
 public class HolaMundo {
 
-  
+    // ¡Solo dejamos un main!
     public static void main(String[] args) {
-        int num1, num2, num3 ;
+        int edad = 0;
+        int membresia = 0;
         
-     num1 =0;
-     num2=3;
-     
-        if(num2>num1){
-            System.out.println("el numero 2 es mayor que el numero 1");
-           
-        }
-        else{
-            if(num1==num2){
-                System.out.println("los numeros son iguales");
-            }
-            else{
-                if(num1==0){
-                    System.out.println("el numero es igual a cero");
-                }
-                else{
-                    System.out.println("el numero 1 es mayor");
-                }
-            }
-        }
+        Scanner leer = new Scanner(System.in);
+        System.out.println("ingrese su edad:");
+        edad = leer.nextInt();
         
-     
-    }
-    
+        if (edad >= 18) {
+            System.out.println("eres mayor de edad");
+            System.out.println("tienes membresia?");
+            System.out.println("1) si");
+            System.out.println("2) no");
+            membresia = leer.nextInt();
+            
+            if (membresia == 1) {
+                System.out.println("Eres Adulto VIP: Tienes acceso a la sala 3D y barra libre de snacks.");
+            } else {
+                System.out.println("Eres Adulto Estándar: Tienes acceso a la sala normal.");
+            }
+            
+        } else {
+            System.out.println("eres menor de edad");
+            System.out.println("tienes membresia?");
+            System.out.println("1) si");
+            System.out.println("2) no");
+            membresia = leer.nextInt();
+            
+            if (membresia == 1) {
+                System.out.println("Eres Joven VIP: Tienes acceso a la sala de videojuegos antes de la película.");
+            } else {
+                System.out.println("Eres Joven Estándar: Tienes acceso a la sala normal.");
+            }
+        }
+    }      
 }
